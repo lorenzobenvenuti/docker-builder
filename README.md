ff8c5bd51d861750908a1a9da6a57caa7cbecf5d
Lorenzo Benvenuti <lorenzobenvenuti@gmail.com>
2017-04-04 11:26:37 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# docker-builder
+
+A simple tool to build Docker images.
+
+In some cases you need to build different variations of the same Docker image. For instance, you may want to test a JEE application with different JDKs and application servers versions: in this case you can configure your build using a yaml file and build/tag all the variations automatically.
+
+```
+name: my-registry:5000/lorenzobenvenuti/wildlfy
+dir: /path/to/dockerfile/dir
+images:
+  - name: 9.0.2
+    tags:
+      - 9.0.2.Final-jdk8u112
+      - 9.0.2.Final-jdk8
+      - 9-jdk8
+      - 9
+    args:
+      JDK_VERSION: 8u112
+      WILDFLY_VERSION: 9.0.2.Final
+  - name: 10.1.0
+    tags:
+      - 10.1.0.Final-jdk8u112
+      - 10-jdk8
+      - 10.1.0.Final-jdk8
+      - 10
+    args:
+      JDK_VERSION: 8u112
+      WILDFLY_VERSION: 10.1.0.Final
+```
+
+```bash
+$ docker-builder.py build wildfly.yaml
+```
+
+To push the images:
+
+```bash
+$ docker-builder.py push wildfly.yaml
+```
+
+You can specify different build directories using a `dir` entry for each image.
+You can specify which images to build using the `--image`/`-i` option.
---
+++ docker-builder.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+import argparse
+import yaml
+import docker
+import json
+import sys
+
+
+def to_str_dictionary(d):
+    return {k: str(v) for k, v in d.items()}
+
+
+def build(name, directory, images):
+    client = docker.from_env(version='auto')
+    for image in images:
+        tags = image['tags']
+        main_tag = "{}:{}".format(name, tags[0])
+        curr_dir = image.get('dir', None) or directory
+        print "Building image {}".format(main_tag)
+        img = client.images.build(path=curr_dir,
+                                  tag=main_tag,
+                                  buildargs=to_str_dictionary(image['args']),
+                                  rm=True)
+        for tag in tags[1:]:
+            print "Tagging image {} -> {}".format(main_tag, tag)
+            img.tag(name, tag)
+
+
+def push(name, images):
+    client = docker.from_env(version='auto')
+    for image in images:
+        for tag in image['tags']:
+            print "Pushing image {}:{}".format(name, tag)
+            client.images.push("{}:{}".format(name, tag))
+
+
+def filter_images(images, selected):
+    if not selected:
+        return images
+    return [i for i in images if i['name'] in selected]
+
+
+def build_images(args):
+    for curr_file in args.file:
+        with open(curr_file, 'r') as yaml_file:
+            data = yaml.load(yaml_file.read())
+            build(data['name'], data.get('dir', None),
+                  filter_images(data['images'], args.image))
+
+
+def push_images(args):
+    for curr_file in args.file:
+        with open(curr_file, 'r') as yaml_file:
+            data = yaml.load(yaml_file.read())
+            push(data['name'], filter_images(data['images'], args.image))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='A tool for building docker images'
+    )
+    subparsers = parser.add_subparsers()
+    parser_build = subparsers.add_parser('build', help='Builds images')
+    parser_build.add_argument('file', nargs='+', help='Configuration file')
+    parser_build.add_argument('--image', '-i', action='append',
+                              help='Selects images to build')
+    parser_build.set_defaults(func=build_images)
+    parser_push = subparsers.add_parser('push', help='Push images')
+    parser_push.add_argument('file', nargs='+', help='Configuration file')
+    parser_push.add_argument('--image', '-i', action='append',
+                             help='Selects images to push')
+    parser_push.set_defaults(func=push_images)
+    args = parser.parse_args()
+    args.func(args)
